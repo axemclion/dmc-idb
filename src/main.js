dc75c81d79b89b6cf08d69112a3a67d224d92a81
Parashuram <code@r.nparashuram.com>
2013-06-06 16:03:32 +0200

    Separated fetch and save

--- src/main.js
+++ src/main.js
@@ -1,38 +1,49 @@
 //var sessions = 'http://www.mobileconference.nl/schedule.json&mobile=true&loadother=true';
 
 $(document).ready(function() {
-	$.getJSON('../data/sessions.json', function(data) {
-		var items = [];
-		$.each(data.sessions, function(key, val) {
-			$.each(val, function() {
-				$.each(this, function(key, val) {
-					items.push(val);
-				});
-			});
+	$(document).trigger('displayData');
+	$.getJSON('../data1/sessions.json').done(function(data) {
+		writeData(data, function(){
+			// TODO: Write better logic to check if the data has changed
+			$(document).trigger('displayData');
 		});
-
-		function addItems(i) {
-			// SAVE TO Database
-			$.indexedDB('dmc').objectStore('sessions', true).add(items[i]).always(function() {
-				if (i < items.length) {
-					addItems(i + 1);
-				} else {
-					$(document).trigger('displayData');
-				}
-			});
-		}
-		addItems(0);
 	});
 });
 
+
+function writeData(data, callback) {
+	var items = [];
+	$.each(data.sessions, function(key, val) {
+		$.each(val, function() {
+			$.each(this, function(key, val) {
+				items.push(val);
+			});
+		});
+	});
+
+	function addItems(i) {
+		// SAVE TO Database
+		$.indexedDB('dmc').objectStore('sessions', true).add(items[i]).always(function() {
+			if (i < items.length) {
+				addItems(i + 1);
+			} else {
+				// Called when all data is written
+				callback();
+			}
+		});
+	}
+	addItems(0);
+}
+
+
 $(document).on('displayData', function() {
 	var ul = $('<ul>');
-	$.indexedDB('dmc').objectStore('sessions').each(function(data){
+	$.indexedDB('dmc').objectStore('sessions').each(function(data) {
 		$('<li>').html(showSession(data.value)).appendTo(ul);
 	});
 	$('#content').append(ul);
 });
 
-function showSession(s){
+function showSession(s) {
 	return s.day + " " + s.time + " - " + s.title;
 }
