6ff2a9eb67edceca6cdf3f0b90635b3db4ca44d5
Parashuram <code@r.nparashuram.com>
2013-06-06 14:08:59 +0200

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				jshintrc: __dirname + '/.jshintrc'
+			},
+			files: ['src/**/*.js']
+		},
+		connect: {
+			server: {
+				options: {
+					base: '.',
+					port: 9999
+				}
+			}
+		},
+		watch: {}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('dev', ['jshint', 'connect', 'watch']);
+	grunt.registerTask('default', 'dev');
+}
--- README.md
+++ README.md
@@ -2,3 +2,11 @@ dmc-idb
 =======
 
 Tutorial Session - http://www.mobileconference.nl/tutorials#parashuram-get-off-my-internets-building-offline-web-applications-tutorial
+
+
+URLs
+=====
+
+* Session Data - http://www.mobileconference.nl/schedule.json&mobile=true&other=true
+* 
+
